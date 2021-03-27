ba61daf9af6fa059a4bfc81d5a9070263684dc09
Nikavocado <77246165+Nikavocado@users.noreply.github.com>
2021-03-27 12:02:09 -0500

    Add files via upload

---
+++ BlueBlock.js
@@ -0,0 +1,10 @@
+class BlueBlock{
+    constructor(x,y){
+        super(x,y,20,30)
+    }
+display(){
+    fill(85,136,238);
+    
+}
+
+}
---
+++ GreenBlock.js
@@ -0,0 +1,10 @@
+class GreenBlock{
+    constructor(x,y){
+        super(x,y,20,30)
+    }
+display(){
+    fill(75,200,206);
+    
+}
+
+}
